f393d55a73d339991c2e5f48ffb9c1e3698de33a
hancrysta1 <hancrystal@sookmyung.ac.kr>
2023-02-01 22:34:47 +0900

    start

---
+++ src/main/java/allba/allbaback/AllbaBackApplication.java
@@ -0,0 +1,13 @@
+package allba.allbaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllbaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllbaBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/allba/allbaback/AllbaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package allba.allbaback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllbaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
