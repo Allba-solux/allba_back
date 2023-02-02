f5e0dbf0cf248b72ce0f586c6fd01a4685a20bbf
gaeunlee <0923ule@gmail.com>
2023-02-02 15:08:13 +0900

    [CREATE] 지점 관리
    -지점 등록
    -지점 조회
    -지점 검색
    -지점 삭제

---
+++ allbaback/src/main/java/freshman/allbaback/domain/Company.java
@@ -0,0 +1,23 @@
+package freshman.allbaback.domain;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+import java.util.List;
+@Data
+@Getter @Setter
+@Document(collection = "company")
+public class Company {
+    @MongoId
+    private String companyCode;
+
+    private String companyName;
+    private String companyPW;
+    private String companyDescription;
+
+    //private Scheduler companySchedule;
+    //private List<Member> employee;
+}
