46fcd613991a477e2ee0f1f677681fa18d3cdfc9
gaeunlee <0923ule@gmail.com>
2023-02-02 13:13:49 +0900

    [INIT] reinitialize

--- allbaback/src/main/java/freshman/allbaback/AllbabackApplication.java
+++ allbaback/src/main/java/freshman/allbaback/AllbabackApplication.java
@@ -1,13 +1,13 @@
-package allba.allbaback;
+package freshman.allbaback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AllbaBackApplication {
+public class AllbabackApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AllbaBackApplication.class, args);
+		SpringApplication.run(AllbabackApplication.class, args);
 	}
 
 }
--- allbaback/src/test/java/freshman/allbaback/AllbabackApplicationTests.java
+++ allbaback/src/test/java/freshman/allbaback/AllbabackApplicationTests.java
@@ -1,10 +1,10 @@
-package allba.allbaback;
+package freshman.allbaback;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AllbaBackApplicationTests {
+class AllbabackApplicationTests {
 
 	@Test
 	void contextLoads() {
